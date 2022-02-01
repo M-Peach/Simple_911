311c97f1bf52060fc9fe03cb327a31d995916b99
M-Peach <mikepeachman@live.com>
2022-01-31 21:22:29 -0500

    Add project files.

---
+++ Simple_911/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Simple_911.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
